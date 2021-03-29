e8eca48da1b51fe1546cf1b3f17f502c5364652e
Dtkrontz <dtkrontz@gmail.com>
2021-03-29 12:08:53 -0400

    add style to NYT

--- src/Components/NYT-Results.js
+++ src/Components/NYT-Results.js
@@ -8,16 +8,17 @@ const Result = (props) => {
             {result.map((result) => {
                 return (
                 <div>
-                    <a href={result.web_url} target='blank'><h2>{result.headline.main}</h2></a>
+                    <h2><a href={result.web_url} target='blank' style={{textDecoration: 'none', color: 'black'}} >{result.headline.main}</a></h2>
                     {result.multimedia[0] ? <img src={`https://www.nytimes.com/${result.multimedia[0].url}`} alt='article img' /> : <p>No Image Found</p>}
-                    <h4>Keywords:</h4>
+                    {result.keywords[0] ? <div><h4>Keywords:</h4>
                     <p>{result.keywords.map(words => {
                         return (
                             <div>
                                 <li>{words.value}</li>
                             </div>
                         )
-                    })}</p>
+                    })}</p></div> : <h4>No Keywords Found</h4>}
+                    
                 <hr />
                 </div>
             )}
